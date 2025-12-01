352d98b20c20e635ed24f565d1d113fe048080ac
maibui7879 <maibui7879@gmail.com>
2025-12-01 14:43:38 +0700

    y

--- src/pages/dashboard/chat/index.tsx
+++ src/pages/dashboard/chat/index.tsx
@@ -117,7 +117,7 @@ export default function ChatPage() {
     if (sessionIdFromUrl) {
       loadMessages(sessionIdFromUrl)
     }
-  }, [sessionIdFromUrl])
+  }, [sessionIdFromUrl]) //sybau
 
   useEffect(() => {
     scrollToBottom()
